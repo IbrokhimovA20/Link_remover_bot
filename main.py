608796c9c8b56406c9230de9e6e252d6d7b50eca
Akhrorbek Ibrokhimov <ibrokhimovakhrorbek20@gmail.com>
2025-04-07 16:14:42 +0500

    added requirement file

--- main.py
+++ main.py
@@ -3,10 +3,5 @@ from loader import dp
 import handlers
 
 
-async def on_startup(dispatcher):
-    await set_default_command(dispatcher)
-
-    await on_startup_notify(dispatcher)
-
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp,skip_updates=True)
